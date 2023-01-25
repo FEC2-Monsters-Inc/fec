58fccc31d4cc431b82e8b8ffc64854c516a5ba3e
boltonlin <boltonlinbo@gmail.com>
2023-01-25 15:26:56 -0800

    boilerplate for front end should be done

---
+++ client/src/components/qanda/QandA.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import fetcher from '../../fetchers';
+
+export default function QandA() {
+
+  return (
+    <div>
+      Questions and Answers
+    </div>
+  );
+}

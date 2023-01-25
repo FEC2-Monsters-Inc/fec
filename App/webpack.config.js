d2c2b3d9f0371be6f12676d28a99a6571e3a857d
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-01-25 12:15:03 -0800

    basic skeleton

---
+++ App/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './App/client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+  ]
+  },
+  mode: 'development'
+}

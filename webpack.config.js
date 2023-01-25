81bdf4f02f229d8000b347d3b2b9051544e4826b
boltonlin <boltonlinbo@gmail.com>
2023-01-25 11:40:11 -0800

    added boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'client/src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'client/dist');
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /nodeModules/,
+      use: {
+        loader: "babel-loader"
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+    }
+  ]
+}

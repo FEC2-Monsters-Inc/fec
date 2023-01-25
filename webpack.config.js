3b061787a9d9b1d621a56cfe7acd82aba888268a
boltonlin <boltonlinbo@gmail.com>
2023-01-25 12:05:19 -0800

    added @babel/preset-react and set preset to compile jsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,24 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'client/src/index.jsx'),
   output: {
-    path: path.resolve(__dirname, 'client/dist');
+    path: path.resolve(__dirname, 'client/dist'),
     publicPath: '',
     filename: 'bundle.js'
   },
   mode: 'development',
 
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /nodeModules/,
-      use: {
-        loader: "babel-loader"
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      },
-    }
-  ]
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      }
+    ]
+  }
 }

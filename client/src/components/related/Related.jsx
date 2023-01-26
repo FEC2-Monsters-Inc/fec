3b1cb3e78b432b740fcb64aa577f986389e482a4
Zhixiang <zhixiang.peng@gmail.com>
2023-01-26 12:44:13 -0800

    Fn to Fetch Related Products Info & Fn to Fetch Product Styles

--- client/src/components/related/Related.jsx
+++ client/src/components/related/Related.jsx
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import fetcher from '../../fetchers';
+import axios from 'axios';
+import RelatedList from './RelatedList.jsx';
 
-export default function Related({
-  feature
-}) {
+export default function Related({ feature, getRelatedList }) {
+  const [relatedList, setRelatedList] = useState([]);
+
+  useEffect(() => {
+    fetcher.related.getRelatedProduct(`${feature.id}`)
+      .then(({ data }) => {
+        if (!data.length) {
+          throw new Error('No Related Product!');
+        }
+        return Promise.all(data.map(id => fetcher.getProductById(id)));
+      })
+      .then(results => setRelatedList(results.map(result => result.data)))
+      .catch(err => console.log(err));
+  }, [feature]);
 
   return (
     <div>
-      Related Items & Comparison
+      <RelatedList relatedList={relatedList} />
     </div>
   );
 }

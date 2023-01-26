4a9e47fd3cf2f16b3028af3178fffc9b58d23981
Zhixiang <zhixiang.peng@gmail.com>
2023-01-26 12:50:29 -0800

    Revert "Fn to Fetch Related Products Info & Fn to Fetch Product Styles"
    This reverts commit 3b1cb3e78b432b740fcb64aa577f986389e482a4.

--- client/src/components/related/Related.jsx
+++ client/src/components/related/Related.jsx
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import fetcher from '../../fetchers';
-import axios from 'axios';
-import RelatedList from './RelatedList.jsx';
 
-export default function Related({ feature, getRelatedList }) {
-  const [relatedList, setRelatedList] = useState([]);
-
-  useEffect(() => {
-    fetcher.related.getRelatedProduct(`${feature.id}`)
-      .then(({ data }) => {
-        if (!data.length) {
-          throw new Error('No Related Product!');
-        }
-        return Promise.all(data.map(id => fetcher.getProductById(id)));
-      })
-      .then(results => setRelatedList(results.map(result => result.data)))
-      .catch(err => console.log(err));
-  }, [feature]);
+export default function Related({
+  feature
+}) {
 
   return (
     <div>
-      <RelatedList relatedList={relatedList} />
+      Related Items & Comparison
     </div>
   );
 }

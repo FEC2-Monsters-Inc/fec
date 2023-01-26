4ae2bd510fef16ea2194ca713f3ced5c10c026f9
Jerrod Varney <jerrodvarney@gmail.com>
2023-01-26 13:55:09 -0800

    created stubs for overview sub-components, added assts file to dist with a new sitewide favicon. updated index.html to show new favicon. Added style fetcher in overview fetcher file. Created state data and initialization function within Overview.jsx.

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,13 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import fetcher from '../../fetchers';
 
+///// SUB-COMPONENETS /////
+import Search from './sub-comps/Search.jsx';
+import Gallery from './sub-comps/Gallery.jsx';
+import ProdInfo from './sub-comps/ProdInfo.jsx';
+import StyleSelect from './sub-comps/StyleSelect.jsx';
+import AddCart from './sub-comps/AddCart.jsx';
+import Description from './sub-comps/Description.jsx';
+
+
 export default function Overview({
-  feature
+  feature,
+  setFeature
 }) {
 
+  ///// STATE DATA /////
+  const [prodStyle, setStyle] = useState({});
+  const [description, setDisplay] = useState(true);
+
+  console.log('feature: ', feature);
+  console.log('style: ', prodStyle);
+
+
+
+  ///// INITIALIZATION /////
+  useEffect(() => {
+    fetcher.overview.getStylesById(40344)
+      .then(result => setStyle(result.data))
+      .catch(err => console.log('err initial styles fetch: ', err));
+  }, []);
+
   return (
-    <div>
-      Overview
-    </div>
+    <>
+      <div id="header">
+        <h1 className="temp-logo">FEC Project</h1>
+        <Search />
+      </div>
+      <div id="overview">
+        <Gallery />
+        <ProdInfo />
+        <StyleSelect />
+        <AddCart />
+        { description
+          ? <Description />
+          : null}
+      </div>
+    </>
   );
 }
---
+++ client/src/components/overview/sub-comps/ProdInfo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProdInfo(props) {
+
+  return (
+    <div id="prod-info">
+      <h3>PRODUCT INFO COMPONENT</h3>
+    </div>
+  );
+}

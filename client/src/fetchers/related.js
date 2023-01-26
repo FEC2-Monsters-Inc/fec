0dd3012a9c3967753dead109fac1c29916c6ea67
Zhixiang <zhixiang.peng@gmail.com>
2023-01-26 12:49:12 -0800

    Revert "Add Related and Style to Fetcher.related"
    This reverts commit d4104088f35ebe718a97ed7560c46066b3dcefd6.

--- client/src/fetchers/related.js
+++ client/src/fetchers/related.js
@@ -1,24 +1,6 @@
 import axios from 'axios';
 
-const SERVER_BASEURL = 'http://localhost:3000/api';
-
 export default {
   // YOUR REQUESTS HERE
-  getRelatedProduct: (id) => {
-    let options = {
-      url: SERVER_BASEURL + `/products/${id}/related`,
-      method: 'get'
-    };
 
-    return axios(options);
-  },
-
-  getProductStyle: (id) => {
-    let options = {
-      url: SERVER_BASEURL + `/products/${id}/styles`,
-      method: 'get'
-    };
-
-    return axios(options);
-  }
 };

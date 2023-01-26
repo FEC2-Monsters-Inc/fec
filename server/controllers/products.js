b69107ebfc1e09d53bfe5072db38e36d47cc04ae
boltonlin <boltonlinbo@gmail.com>
2023-01-25 19:28:26 -0800

    better error tracking

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -4,12 +4,12 @@ module.exports = {
   getProducts: (req, res) => {
     model.getProducts()
       .then(results => res.status(200).send(results.data))
-      .catch(err => console.log('error getProducts: ', err));
+      .catch(err => console.log('err ctrl.getProducts: ', err));
   },
 
   getProductById: (req, res) => {
     model.getProductById(req.params.product_id)
       .then(results => res.status(200).send(results.data))
-      .catch(err => console.log('error getProductById: ', err));
+      .catch(err => console.log('err ctrl.getProductById: ', err));
   }
 };

2367dfe9e55d648f36cfa64dbdcaa726cd11ddfe
Erik Newland <114263701+eriknewland@users.noreply.github.com>
2023-01-26 14:46:26 -0800

    Update server/controllers/reviews.js
    Co-authored-by: Bolton Lin <boltonlinbo@gmail.com>

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -4,7 +4,7 @@ module.exports = {
   getReviews: (req, res) => {
     model.getReviews(req.params.product_id)
       .then(results => {
-        console.log(results);
+        .then(results => res.status(200).send(results.data))
         res.status(200).send(results.data);
       })
       .catch(err => console.log('err ctrl.getReviews: ', err));

36b0c13a3e679dffc6f8b354ee9fe0d5911cca0f
Zhixiang <zhixiang.peng@gmail.com>
2023-01-26 16:13:03 -0800

    Complete the API for Products and Reviews

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,5 +1,53 @@
 const model = require('../models').reviews;
 
 module.exports = {
+  getReviews: (req, res) => {
 
+    model.getReviews(req.query)
+      .then(({data}) => res.status(200).send(data))
+      .catch(err => {
+        console.log('err ctrl.getReviews: ', err);
+        res.status(500).send(err);
+      });
+  },
+
+  getReviewMeta: (req, res) => {
+
+    model.getReviewMeta(req.query)
+      .then(({data}) => res.status(200).send(data))
+      .catch(err => {
+        console.log('err ctrl.getReviewMeta: ', err);
+        res.status(500).send(err);
+      });
+  },
+
+  addReviews: (req, res) => {
+
+    model.addReviews(req.body)
+      .then(({config}) => res.status(201).send(config.data))
+      .catch(err => {
+        console.log('err ctrl.addReviews: ', err);
+        res.status(500).send(err);
+      });
+  },
+
+  updateUseful: (req, res) => {
+
+    model.updateUseful(req.params.review_id)
+      .then(() => res.status(201).send('Useful Updated'))
+      .catch(err => {
+        console.log('err ctrl.updateUseful: ', err),
+        res.status(204).send(err);
+      });
+  },
+
+  updateReport: (req, res) => {
+
+    model.updateReport(req.params.review_id)
+      .then(() => res.status(201).send('Report Update'))
+      .catch(err => {
+        console.log('err ctrl.updateReport: ', err),
+        res.status(204).send(err);
+      });
+  },
 };

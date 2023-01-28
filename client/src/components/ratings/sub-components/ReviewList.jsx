172088a115bd39cdc1e811c3f2b8ca3d863062cf
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-01-28 13:40:17 -0800

    finished tracker function to accurately track number of reviews displayed across various filters

--- client/src/components/ratings/sub-components/ReviewList.jsx
+++ client/src/components/ratings/sub-components/ReviewList.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReviewTile from './ReviewTile.jsx';
 
 export default function ReviewList({reviews, selectedRating}) {
 
   // consider using hook to track length of current reviews
-  //const [numReviews, setNumReviews] = useState(reviews.length);
+  const [numReviews, setNumReviews] = useState(reviews.length);
   // TESTING W BOLTON //
+
 
   const reviewMapper = reviews.map((review, index) =>
     <ReviewTile review={review} key={index}/>
@@ -14,22 +15,37 @@ export default function ReviewList({reviews, selectedRating}) {
   const filterReviewMapper = () => {
     const filteredReviews = reviews.filter(review => selectedRating[review.rating] === true);
     if (filteredReviews.length === 0) {
+      //setNumReviews(reviewMapper.length);
       return reviewMapper;
     } else {
+      //setNumReviews(filteredReviews.length);
       return filteredReviews.map((review, index) =>
         <ReviewTile review={review} key={index}/>
       );
     }
   };
 
+  const numReviewsTracker = function() {
+    let trueKeys = Object.keys(selectedRating).filter(key => selectedRating[key] === true).map(Number);
+    let filteredArr = reviews.filter(review => trueKeys.includes(review.rating));
+    //console.log(filteredArr)
+    if (filteredArr.length === 0) {
+      //console.log('test')
+      setNumReviews(reviews.length); //FIX HARD CODED SOLUTION
+      return;
+    }
+    setNumReviews(filteredArr.length);
+  };
+
+  useEffect(() => {
+    numReviewsTracker();
+  }, [selectedRating, reviews]);
+
 
   return (
     <div className="review-list-container">
-      <p>{reviews.length} reviews, sorted by relevance</p>
+      <p>{numReviews} reviews, sorted by relevance</p>
       {filterReviewMapper()}
-      <div>
-        {console.log(document.getElementsByClassName('review-list-container'))}
-      </div>
     </div>
   );
 }

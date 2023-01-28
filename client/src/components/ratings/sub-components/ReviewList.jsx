b5982f24dcd2cae810e0f9389035c9354de15275
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-01-28 09:49:16 -0800

    final commit ahead of pr

--- client/src/components/ratings/sub-components/ReviewList.jsx
+++ client/src/components/ratings/sub-components/ReviewList.jsx
@@ -3,10 +3,13 @@ import ReviewTile from './ReviewTile.jsx';
 
 export default function ReviewList({reviews, selectedRating}) {
 
+  // consider using hook to track length of current reviews
+  //const [numReviews, setNumReviews] = useState(reviews.length);
+
   const reviewMapper = reviews.map((review, index) =>
     <ReviewTile review={review} key={index}/>
   );
-  //console.log(selectedRating);
+
   const filterReviewMapper = () => {
     const filteredReviews = reviews.filter(review => selectedRating[review.rating] === true);
     if (filteredReviews.length === 0) {
@@ -23,6 +26,9 @@ export default function ReviewList({reviews, selectedRating}) {
     <div className="review-list-container">
       <p>{reviews.length} reviews, sorted by relevance</p>
       {filterReviewMapper()}
+      <div>
+        {console.log(document.getElementsByClassName('review-list-container'))}
+      </div>
     </div>
   );
 }

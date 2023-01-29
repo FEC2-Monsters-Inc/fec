3d1d22acbb51549131d8ccfb4d0e0726134a1c35
Zhixiang <zhixiang.peng@gmail.com>
2023-01-28 22:02:26 -0800

    Add Star Rating to Related Product Component

--- client/src/components/related/related-components/RelatedProduct.jsx
+++ client/src/components/related/related-components/RelatedProduct.jsx
@@ -1,24 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import fetcher from '../../../fetchers';
+import axios from 'axios';
 import CompareModal from './CompareModal.jsx';
-import { AiOutlineStar } from 'react-icons/ai';
+import { AiOutlineStar, AiTwotoneStar } from 'react-icons/ai';
+import StarRating from './StarRating.jsx';
 
 export default function RelatedProduct({ start, last, feature, relProd }) {
 
   const [image, setImage] = useState();
   const [showModal, setShowModal] = useState(false);
   const [showImg, setShowImg] = useState(false);
+  const [ratings, setRatings] = useState(0);
 
   useEffect(() => {
-    fetcher.related.getProductStyle(relProd.id)
-      .then(({ data }) => setImage(data.results[0]))
-      .catch(err => console.log(err));
+    axios.all([
+      fetcher.related.getProductStyle(relProd.id),
+      fetcher.related.getReviewMeta(relProd.id)
+    ])
+      .then(axios.spread((...data) => {
+        // console.log(data[0].data.results[0]);
+        setImage(data[0].data.results[0]);
+        // console.log(data[1].data);
+        setRatings(data[1].data);
+      }))
+      .catch(err => console.error(err));
+    // fetcher.related.getProductStyle(relProd.id)
+    //   .then(({ data }) => setImage(data.results[0]))
+    //   .catch(err => console.log(err));
   }, [feature]);
 
   // TODO: FIX NO IMAGE LATER
   if (!image || !image.photos[0].thumbnail_url) {
     return <div></div>;
   }
+
+  console.log(ratings);
 
   return (
     <div>
@@ -32,6 +48,7 @@ export default function RelatedProduct({ start, last, feature, relProd }) {
         <div className="rel-slogan">{relProd.slogan}</div>
         {image.sale_price && <div className="rel-sale-price">{'$' + image.sale_price}</div>}
         <div className="rel-orig-price">{'$' + image.original_price}</div>
+        <StarRating ratingPercentage={'50%'}></StarRating>
       </div>
       {showModal &&
         (<div className="overlay">

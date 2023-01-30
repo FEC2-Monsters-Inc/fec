31b20ac0e1475917621eba8632466a7a313059e3
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-01-30 13:20:41 -0800

    review image modal and summary truncation with expandable elipses complete

--- client/src/components/ratings/sub-components/ReviewTile.jsx
+++ client/src/components/ratings/sub-components/ReviewTile.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReviewImageModal from './ReviewImageModal.jsx';
 
 export default function ReviewTile({ review }) {
+  const [modalToggle, setModalToggle] = useState(false);
+  const [imgString, setImgString] = useState('');
+  const [showFull, setShowFull] = useState(false);
   const starRater = () => {
     let stars = '';
     for (let i = 0; i < review.rating; i += 1) {
@@ -19,17 +23,68 @@ export default function ReviewTile({ review }) {
     return `${monthName} ${day}, ${year}`;
   };
 
+  const nameAndDate = `${getDateString(review.date)}, ${review.reviewer_name}`;
+  const summaryLengthChecker = () => {
+    if (review.summary.length > 60) {
+      return `${review.summary.substring(0, 60)} ...`;
+    }
+    return review.summary;
+  };
+  const elipsesSpan = () => (
+      <span onClick={() => setShowFull(true)}>
+        {showFull ? review.body : '...'}
+      </span>
+  );
+  const bodyLengthChecker = () => {
+    if (review.body.length > 250) {
+      return `${review.body.substring(0, 250)}`;
+    }
+    return review.body;
+  };
+  const imgToggler = (pic) => {
+    setModalToggle(!modalToggle);
+    setImgString(pic);
+  };
+  const photoHandler = () => {
+    if (review.photos.length > 0) {
+      const photoAltTxt = `Image for review titled ${review.summary}`;
+      return review.photos.map((element) => (
+        <img
+          src={element.url}
+          alt={photoAltTxt}
+          key={element.id}
+          className="review-tile-individual-photo"
+          onClick={()=>imgToggler(element.url)}
+        />
+      ));
+    }
+  };
+  useEffect(() => {
+    if (review.body.length < 250) {
+      setShowFull(true);
+    }
+  });
+
   return (
-    <div className="review-tile-main-container">
+    <div className="review-tile-main-container">{console.log(review.photos)}
       <div>
         <div className="review-tile-container-1">
-          <div className="review-tile-summary">{review.summary}</div>
+          <div className="review-tile-nameAndDate">{nameAndDate}</div>
           <div className="review-tile-stars">{starRater()}</div>
         </div>
-        <p>{review.body}</p>
+        <div className="review-tile-summary">{summaryLengthChecker()}</div>
+        <p className="review-tile-body">
+          {bodyLengthChecker()}
+          {showFull ? review.body.substring(250) : elipsesSpan()}
+        </p>
+        <div className="review-tile-photos-container">{photoHandler()}</div>
         <div className="review-tile-container-2">
           <div className="review-tile-name">{review.reviewer_name}</div>
           <div className="review-tile-date">{getDateString(review.date)}</div>
+          <div>
+            {modalToggle
+            && <ReviewImageModal imgString={imgString} setModalToggle={setModalToggle} />}
+          </div>
         </div>
       </div>
     </div>

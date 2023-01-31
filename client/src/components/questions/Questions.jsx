2d5765c5b74c6ea28ed1a1840a931174754b1708
boltonlin <boltonlinbo@gmail.com>
2023-01-31 11:44:44 -0800

    minor style changes

--- client/src/components/questions/Questions.jsx
+++ client/src/components/questions/Questions.jsx
@@ -9,18 +9,42 @@ export default function Questions({
 }) {
   const [questions, setQuestions] = useState([]);
   const [filterText, setFilterText] = useState('');
+  const [timer, setTimer] = useState(null);
 
-  useEffect(() => {
+  const onFilterTextChange = (value) => {
+    setFilterText(value);
+    clearTimeout(timer);
+  };
+
+  const putQuestions = () => {
     fetcher.questions.getById(feature.id)
       .then(({ data }) => setQuestions(data.results))
       .catch((err) => console.error('Questions on feature change fetch: ', err));
+  };
+
+  // TODO: investigate react-hooks/exhaustive-deps
+  useEffect(() => {
+    putQuestions();
   }, [feature]);
 
+  useEffect(() => {
+    if (filterText.length >= 3) {
+      setTimer(setTimeout(() => {
+        // TODO: filter the questions
+        console.log("I'M SEARCHING");
+      }, 500));
+    }
+  }, [filterText]);
+
   return (
-    <div className="qa section">
+    <div className="qa qa-section">
       <h2>QUESTIONS & ANSWERS</h2>
-      <SearchBar text={filterText} handleChange={setFilterText} />
-      <QuestionsList product_id={feature.id} questions={questions} />
+      <SearchBar text={filterText} handleChange={onFilterTextChange} />
+      <QuestionsList
+        product_id={feature.id}
+        questions={questions}
+        updateQuestions={putQuestions}
+      />
     </div>
   );
 }

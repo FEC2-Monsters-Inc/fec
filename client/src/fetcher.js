8fdb3bc82fe22947e3dae37079c5ffeb317acaeb
boltonlin <boltonlinbo@gmail.com>
2023-02-01 14:21:57 -0800

    moved react-test-renderer to dev dependency

---
+++ client/src/fetcher.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const SERVER_BASEURL = 'http://localhost:3000/api';
+
+export default {
+  getProductById: (id) => {
+    const options = {
+      url: `${SERVER_BASEURL}/products/${id}`,
+      method: 'get',
+    };
+
+    return axios(options);
+  },
+
+  getProductStyle: (id) => {
+    const options = {
+      url: `${SERVER_BASEURL}/products/${id}/styles`,
+      method: 'get',
+    };
+
+    return axios(options);
+  },
+
+
+};

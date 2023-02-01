6876f299041ff4ee3a96aa61754f81d3ca675fb3
Jerrod Varney <jerrodvarney@gmail.com>
2023-02-01 13:23:36 -0800

    fixed gallery styling for overview component.

--- client/src/components/overview/sub-comps/Share.jsx
+++ client/src/components/overview/sub-comps/Share.jsx
@@ -4,9 +4,15 @@ import { FaFacebookF, FaTwitter, FaPinterestP } from 'react-icons/fa';
 export default function Share() {
   return (
     <div className="share">
-      <FaFacebookF size="2em" className="social facebook" />
-      <FaTwitter size="2em" className="social twitter" />
-      <FaPinterestP size="2em" className="social pinterest" />
+      <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+        <FaFacebookF size="2em" className="social facebook" />
+      </a>
+      <a href="https://www.pinterest.com/" target="_blank" rel="noreferrer">
+        <FaPinterestP size="2em" className="social pinterest" />
+      </a>
+      <a href="https://twitter.com/?lang=en" target="_blank" rel="noreferrer">
+        <FaTwitter size="2em" className="social twitter" />
+      </a>
     </div>
   );
 }

4ea6e257af2ad2d7e05d6bd977630a84ee70b88b
boltonlin <boltonlinbo@gmail.com>
2023-02-02 11:13:41 -0800

    update tests

--- client/src/components/questions/__tests__/Questions.test.jsx
+++ client/src/components/questions/__tests__/Questions.test.jsx
@@ -15,43 +15,36 @@ beforeEach(() => jest.clearAllMocks());
 afterEach(cleanup);
 
 describe('Questions & Answers Component', () => {
-  it('should render Questions component', async () => {
+  beforeEach(async () => {
     fetcherMock.getQuestionsById.mockResolvedValueOnce({ data: exampleQuestions });
     render(<Questions feature={exampleFeature} />);
+    await screen.findByRole('heading');
+  });
 
-    const header = await screen.findByRole('heading', { level: 2 });
-
+  it('should render the Questions component', () => {
+    const header = screen.getByRole('heading', { level: 2 });
     expect(header).toBeInTheDocument();
   });
 
-  it('should fetch questions on load', async () => {
-    fetcherMock.getQuestionsById.mockResolvedValueOnce({ data: exampleQuestions });
-    render(<Questions feature={exampleFeature} />);
-
-    await waitFor(() => expect(fetcherMock.getQuestionsById).toHaveBeenCalled());
+  it('should fetch questions once on load', async () => {
+    expect(fetcherMock.getQuestionsById).toHaveBeenCalledTimes(1);
+    await screen.findAllByText('Q: ', { exact: false });
+    expect(fetcherMock.getQuestionsById).toHaveBeenCalledTimes(1);
   });
 
   it('should only render 2 questions with answers on load', async () => {
-    fetcherMock.getQuestionsById.mockResolvedValueOnce({ data: exampleQuestions });
-    render(<Questions feature={exampleFeature} />);
-
     const questions = await screen.findAllByText('Q: ', { exact: false });
     expect(questions.length).toBe(2);
   });
 
+  // extract this test into QuestionsList
   it('should render 2 more questions on button click', async () => {
-    fetcherMock.getQuestionsById.mockResolvedValueOnce({ data: exampleQuestions });
-    render(<Questions feature={exampleFeature} />);
-    const user = userEvent.setup();
-
-    const button = await screen.findByRole('button', { name: 'MORE ANSWERED QUESTIONS' });
-    expect(button).toBeInTheDocument();
-
+    const button = screen.getByRole('button', {
+      name: 'MORE ANSWERED QUESTIONS',
+    });
     let questions = await screen.findAllByText('Q: ', { exact: false });
     expect(questions.length).toBe(2);
-
-    await user.click(button);
-
+    await userEvent.click(button);
     questions = await screen.findAllByText('Q: ', { exact: false });
     expect(questions.length).toBe(4);
   });

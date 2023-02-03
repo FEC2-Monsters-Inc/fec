a8bdf238bbce9f749d468c9361a915355c2f324a
Jerrod Varney <jerrodvarney@gmail.com>
2023-02-03 09:25:13 -0800

    added styling and conditional rendering for gallery thumbnails and the price, respectively.

---
+++ client/src/components/Overview/sub-comps/GalleryThumbnails.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import {
+  AiOutlineDoubleLeft, AiOutlineDoubleRight, AiOutlineDown, AiOutlineExpand, AiOutlineUp,
+} from 'react-icons/ai';
+
+export default function GalleryThumbnails({ currStyle }) {
+
+}

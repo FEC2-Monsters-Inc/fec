d181d01027247c3380124d29a176b5a2db5d467b
Jerrod Varney <jerrodvarney@gmail.com>
2023-02-04 14:44:50 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
@@ -11,12 +10,9 @@ const app = express();
 
 /* MIDDLEWARE */
 app.use(morgan('dev'));
-app.use(express.json({
-  limit: '20mb',
-}));
+app.use(express.json()};
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use(cors());
 
 app.use('/api', router);
 

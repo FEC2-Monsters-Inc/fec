b7464b22047db11fbd4c7dc3bd1c9e7b0455c92f
Jerrod Varney <jerrodvarney@gmail.com>
2023-02-04 14:49:44 -0800

    Delete thirdParty.js

--- server/models/thirdParty.js
+++
@@ -1,7 +0,0 @@
-const imgbbUploader = require('imgbb-uploader');
-
-module.exports = {
-  uploadImage: (imgPath) => (
-    imgbbUploader({ apiKey: process.env.IMAGE_BB_APIKEY, base64string: imgPath })
-  ),
-};

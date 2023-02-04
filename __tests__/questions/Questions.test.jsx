868467c82cbe19942435ff7fe355ca4070da96c3
boltonlin <boltonlinbo@gmail.com>
2023-02-03 21:08:02 -0800

    'more' button disappears when no more questions left to render

--- __tests__/questions/Questions.test.jsx
+++ __tests__/questions/Questions.test.jsx
@@ -1,8 +1,7 @@
+/* eslint-disable no-multi-str, no-await-in-loop */
 import React from 'react';
-import {
-  render, screen, cleanup, act,
-} from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { render, screen, act } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import Questions from '../../client/src/components/Questions/Questions.jsx';
 import QuestionsList from '../../client/src/components/Questions/QuestionsList.jsx';
@@ -11,27 +10,22 @@ import mockQuestions from '../example_data/questions/questions';
 import fetcherMock from '../../client/src/fetchers';
 
 jest.mock('../../client/src/fetchers');
+
 beforeEach(() => {
   jest.clearAllMocks();
-});
-afterEach(() => {
-  cleanup();
 });
 
 describe('Questions & Answers Component', () => {
   beforeEach(() => {
-    fetcherMock.getQuestionsById
-      .mockResolvedValueOnce({ data: mockQuestions[40356] });
+    fetcherMock.getQuestionsById.mockResolvedValueOnce({ data: mockQuestions[40356] });
     render(<Questions feature={mockProducts[40356]} />);
   });
 
   it('should fetch questions once on load', async () => {
     expect(fetcherMock.getQuestionsById).toHaveBeenCalledTimes(1);
-    await screen.findAllByText('Q: ', { exact: false });
-    expect(fetcherMock.getQuestionsById).toHaveBeenCalledTimes(1);
   });
 
-  describe('Search Functionality', () => {
+  describe('Search Bar', () => {
     beforeEach(() => {
       jest.useFakeTimers();
     });
@@ -43,28 +37,28 @@ describe('Questions & Answers Component', () => {
 
     it('shouldn\'t filter questions if typing less than 3 characters', async () => {
       const user = userEvent.setup({ delay: null });
-      const searchbar = screen.getByRole('searchbox', {
-        name: /search question/i,
-      });
+      const searchbar = screen.getByRole('searchbox', { name: /search question/i });
       const questionsBefore = await screen.findAllByRole('heading', { name: /q: /i });
+
       await user.type(searchbar, 'te');
       act(() => {
         jest.advanceTimersByTime(500);
       });
+
       const questionsAfter = await screen.findAllByRole('heading', { name: /q: /i });
       expect(questionsBefore).toEqual(questionsAfter);
     });
 
     it('shouldn\'t filter questions before input is debounced', async () => {
       const user = userEvent.setup({ delay: null });
-      const searchbar = screen.getByRole('searchbox', {
-        name: /search question/i,
-      });
+      const searchbar = screen.getByRole('searchbox', { name: /search question/i });
       const questionsBefore = await screen.findAllByRole('heading', { name: /q: /i });
+
       await user.type(searchbar, 'tem');
       act(() => {
         jest.advanceTimersByTime(499);
       });
+
       const questionsAfter = await screen.findAllByRole('heading', { name: /q: /i });
       expect(questionsBefore).toEqual(questionsAfter);
     });
@@ -74,11 +68,14 @@ describe('Questions & Answers Component', () => {
       const searchbar = screen.getByRole('searchbox', {
         name: /search question/i,
       });
+
       await user.type(searchbar, 'tem');
       act(() => {
         jest.advanceTimersByTime(500);
       });
+
       const questions = await screen.findAllByRole('heading', { name: /q: /i });
+
       questions.map((question) => expect(question).toHaveTextContent(/tem/i));
     });
   });
@@ -93,7 +90,8 @@ describe('QuestionsList Component', () => {
   });
 
   it('should only render 2 questions with answers on load', () => {
-    const questions = screen.getAllByText('Q: ', { exact: false });
+    const questions = screen.getAllByRole('heading', { name: /q: /i });
+
     expect(questions.length).toBe(2);
   });
 
@@ -101,10 +99,22 @@ describe('QuestionsList Component', () => {
     const button = screen.getByRole('button', {
       name: 'MORE ANSWERED QUESTIONS',
     });
-    let questions = await screen.findAllByText('Q: ', { exact: false });
-    expect(questions.length).toBe(2);
+
     await userEvent.click(button);
-    questions = await screen.findAllByText('Q: ', { exact: false });
+    const questions = await screen.findAllByRole('heading', { name: /q: /i });
+
     expect(questions.length).toBe(4);
+  });
+
+  it('should hide MORE button when there are no more questions to load', async () => {
+    const button = screen.getByRole('button', { name: /more answered questions/i });
+    const passedQuestions = mockQuestions[40356].results;
+    const numClicks = Math.ceil(passedQuestions.length / 2) - 1;
+
+    for (let i = 0; i < numClicks; i += 1) {
+      await userEvent.click(button);
+    }
+
+    expect(button).not.toBeInTheDocument();
   });
 });

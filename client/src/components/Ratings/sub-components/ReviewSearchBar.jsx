065956f7d2cdb14123d39b695e3415ca231b95fb
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-02-05 20:38:35 -0800

    CSS stylings for very basic animated search bar

--- client/src/components/Ratings/sub-components/ReviewSearchBar.jsx
+++ client/src/components/Ratings/sub-components/ReviewSearchBar.jsx
@@ -1,37 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BsSearch } from 'react-icons/bs';
 
 export default function ReviewSearchBar({
   reviews, searchTerm, setSearchTerm, listIndex,
 }) {
-  // const [searchResults, setSearchResults] = useState([]);
-
   // set search term to the value of the search input
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
+    if (e.target.value === '') {
+      document.querySelector('.search-container').classList.remove('active');
+    } else {
+      document.querySelector('.search-container').classList.add('active');
+    }
   };
 
-  // search through the reviews and add the search results to the state
-  // const handleSearch = (e) => {
-  //   e.preventDefault();
-  //   if (searchTerm.length >= 3) {
-  //     const results = reviews.slice(0, listIndex).filter((review) => {
-  //       const { body, summary, reviewer_name } = review;
-  //       return body.replace(searchTerm, `<span class="highlight">${searchTerm}</span>`) || summary.replace(searchTerm, `<span class="highlight">${searchTerm}</span>`) || reviewer_name.replace(searchTerm, `<span class="highlight">${searchTerm}</span>`);
-  //     });
-  //     setSearchResults(results);
-  //   }
-  // };
-
   return (
-    <form>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleInputChange}
-        placeholder="Search reviews..."
-      />
-      <button type="submit">Search</button>
-
+    <form className="review-search-container">
+      <div className="search-container">
+        <button className="search-button" type="button">
+          <BsSearch />
+        </button>
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search Reviews…"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+      </div>
     </form>
   );
 }
